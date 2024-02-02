b51d9064802a8542d2d1f142c13dd51e0d4d798c
Saurav <sauravc41@gmail.com>
2024-02-01 23:42:31 -0800

    rename file

--- InterfaceSortAlgo.py
+++ InterfaceSortAlgo.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 # Using an abstract class as interface so the sorting algo
 # classes will be forced to implement these methods
-class SortAlgoInterface(ABC):
+class InterfaceSortAlgo(ABC):
 
     @abstractmethod
     def sort(self, arr):
